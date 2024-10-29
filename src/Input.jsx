59dba1a2bdf11ad292b1b9be1299ef8eb03e037f
Khaifkhan <shaikkaif929@gmail.com>
2024-10-29 10:45:44 +0530

    Update Input.jsx

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addTodo, deleteTodo, editTodo } from "./features/todoSlice";
+import {
+  addTodo,
+  deleteTodo,
+  editTodo,
+  toggleComplete,
+} from "./features/todoSlice";
 
 const Input = () => {
   const [text, setText] = useState("");
@@ -33,6 +38,10 @@ const Input = () => {
     dispatch(deleteTodo(id));
   };
 
+  const handleToggleCompletion = (id) => {
+    dispatch(toggleComplete(id));
+  };
+
   return (
     <>
       <div className="bg-slate-800 my-10 mx-56 w-[800px] rounded-md px-10 py-6 gap-5 flex justify-between text-white">
@@ -50,32 +59,79 @@ const Input = () => {
           {editId ? "Update" : "Add"}
         </button>
       </div>
+
       <div className="mt-3 bg-slate-800 w-[50vw] mx-72 rounded-md text-white">
-        {todos.map((ele) => (
-          <div
-            key={ele.id}
-            className="flex py-5 px-5 justify-between items-center"
-          >
-            <li key={ele.id} className="text-white list-none px-5 py-3 ">
-              {ele.text}
-            </li>
-            <div className="flex justify-between gap-5">
-              <button
-                className="bg-green-400 rounded-md h-9 w-20"
-                onClick={() => handleEdit(ele)}
-              >
-                Edit
-              </button>
-              <button
-                className="bg-red-400 rounded-md h-9 w-20"
-                onClick={() => handleDelete(ele.id)}
-              >
-                Delete
-              </button>
+        {todos
+          .filter((e) => !todos.completed)
+          .map((ele) => (
+            <div
+              key={ele.id}
+              className="flex py-5 px-5 justify-between items-center"
+            >
+              <label className="flex gap-5  items-center">
+                <input
+                  type="checkbox"
+                  checked={ele.completed}
+                  onChange={() => handleToggleCompletion(ele.id)}
+                />
+                <span
+                  className={ele.completed ? "line-through text-gray-400" : ""}
+                >
+                  {ele.text}
+                </span>
+              </label>
+              <div className="flex justify-between gap-5">
+                <button
+                  className="bg-green-400 rounded-md h-9 w-20"
+                  onClick={() => handleEdit(ele)}
+                >
+                  Edit
+                </button>
+                <button
+                  className="bg-red-400 rounded-md h-9 w-20"
+                  onClick={() => handleDelete(ele.id)}
+                >
+                  Delete
+                </button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
+
+      {
+        todos.filter((e) => e.completed).length > 0 ? (
+          <div className="mt-3 bg-slate-800 w-[50vw] mx-72 rounded-md text-white">
+            <h2 className="p-2 mx-3 text-lg font-bold">Completed Tasks</h2>
+            {todos
+              .filter((el) => el.completed)
+              .map((ele) => (
+                <div
+                  key={ele.id}
+                  className="flex py-5 px-5 justify-between items-center"
+                >
+                  <label className="flex items-center">
+                    <input
+                      type="checkbox"
+                      checked={ele.completed}
+                      onChange={() => handleToggleCompletion(ele.id)}
+                    />
+                    <span className="line-through text-gray-400">
+                      {ele.text}
+                    </span>
+                  </label>
+                  <div className="flex justify-between gap-5">
+                    <button
+                      className="bg-red-400 rounded-md h-9 w-20"
+                      onClick={() => handleDelete(ele.id)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
+          </div>
+        ) : null 
+      }
     </>
   );
 };
